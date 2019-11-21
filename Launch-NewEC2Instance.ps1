fd24b4123e9aef97c4f7cef9676f40c366005814
Soheil Esmaeili <oilesa@hotmail.com>
2019-11-21 21:01:52 +1100

    Update

--- Launch-NewEC2Instance.ps1
+++ Launch-NewEC2Instance.ps1
@@ -22,20 +22,21 @@ Grant-EC2SecurityGroupIngress -GroupName $SecurityGroupName -IpPermissions @{IpP
 #$InstanceID = New-EC2Instance -ImageId ((Get-SSMParameterValue -Name /aws/service/ami-windows-latest/Windows_Server-2016-English-Full-Base).Parameters[0].Value) -InstanceType t2.small -AssociatePublicIp $true -SubnetId $SubnetID
 
 # Default VPCs
-$SubnetID01 = "subnet-0e540026b17905477" #apse2-az3
-$SubnetID02 = "subnet-0c00851a31a44165c" #apse2-az1
-$SubnetID03 = "subnet-0c19b4eaea76b8f0d" #apse2-az2
+#$SubnetID01 = "subnet-0e540026b17905477" #apse2-az3
+#$SubnetID02 = "subnet-0c00851a31a44165c" #apse2-az1
+#$SubnetID03 = "subnet-0c19b4eaea76b8f0d" #apse2-az2
 # My VPCs
-$SubnetID04 = "subnet-027ff1d8b1aafc88e"  #testnet-private-subnet-01-ap-southeast-2a  apse2-az3
-$SubnetID05 = "subnet-02e10c29f83f452fe"  #testnet-private-subnet-02-ap-southeast-2b  apse2-az1
-$SubnetID06 = "subnet-0c5a0cf2ca91cf7d2"  #testnet-private-subnet-03-ap-southeast-2c  apse2-az2
-$SubnetID07 = "subnet-0703835a956c7d999"  #testnet-public-subnet-01-ap-southeast-2a   apse2-az3
-$SubnetID08 = "subnet-002bc31b3aff87eb2"  #testnet-public-subnet-02-ap-southeast-2b   apse2-az1
-$SubnetID09 = "subnet-0800e4cd7fb42e961"  #testnet-public-subnet-03-ap-southeast-2c   apse2-az2
+#$SubnetID04 = "subnet-027ff1d8b1aafc88e"  #testnet-private-subnet-01-ap-southeast-2a  apse2-az3
+#$SubnetID05 = "subnet-02e10c29f83f452fe"  #testnet-private-subnet-02-ap-southeast-2b  apse2-az1
+#$SubnetID06 = "subnet-0c5a0cf2ca91cf7d2"  #testnet-private-subnet-03-ap-southeast-2c  apse2-az2
+#$SubnetID07 = "subnet-0703835a956c7d999"  #testnet-public-subnet-01-ap-southeast-2a   apse2-az3
+#$SubnetID08 = "subnet-002bc31b3aff87eb2"  #testnet-public-subnet-02-ap-southeast-2b   apse2-az1
+#$SubnetID09 = "subnet-0800e4cd7fb42e961"  #testnet-public-subnet-03-ap-southeast-2c   apse2-az2
+
 # Get the latest AMI
-$ImageID = (Get-SSMParameterValue -Name /aws/service/ami-windows-latest/Windows_Server-2016-English-Full-Base).Parameters[0].Value
+$ImageID = (Get-SSMParameterValue -Name /aws/service/ami-windows-latest/Windows_Server-2019-English-Full-Base).Parameters[0].Value
 # Creating Instance
-$EC2Instance = New-EC2Instance -ImageId $imageid -MinCount 1 -MaxCount 1 -InstanceType t2.micro -KeyName ops-windows-sydney -SecurityGroupId $SecurityGroupID -Monitoring_Enabled $true -SubnetId $SubnetID01
+$EC2Instance = New-EC2Instance -ImageId $imageid -MinCount 5 -MaxCount 5 -InstanceType t3.medium -KeyName ops-windows-sydney -SecurityGroupId $SecurityGroupID -Monitoring_Enabled $true -SubnetId $SubnetID01
 # Tag the Instance
 $Tag = New-Object Amazon.EC2.Model.Tag
 #$Tag.Key = "Name"
--- Test.ps1
+++ Test.ps1
@@ -1,1 +1,19 @@
-New-EC2Instance -ImageId ((Get-SSMParameterValue -Name /aws/service/ami-windows-latest/Windows_Server-2016-English-Full-Base).Parameters[0].Value) -InstanceType t2.small -AssociatePublicIp $true -SubnetId $SubnetID
+#Retrieve the AWS instance ID, keep trying until the metadata is available
+$instanceID = "null"
+while ($instanceID -NotLike "i-*") {
+ Start-Sleep -s 3
+ $instanceID = invoke-restmethod -uri http://169.254.169.254/latest/meta-data/instance-id
+}
+
+$username = "int\eli"
+$password = "QWE752qwe" | ConvertTo-SecureString -AsPlainText -Force
+$cred = New-Object -typename System.Management.Automation.PSCredential($username, $password)
+
+Try {
+#Rename-Computer -NewName $instanceID -Force
+Start-Sleep -s 5
+Add-Computer -DomainName int.cloudifier.com.au -OUPath "CN=Computers,DC=int,DC=cloudifier,DC=com,DC=au" -Options JoinWithNewName,AccountCreate -Credential $cred -Force -Restart -erroraction 'stop'
+}
+Catch{
+echo $_.Exception | Out-File c:\Windows\temp\error-joindomain.txt -Append
+}
